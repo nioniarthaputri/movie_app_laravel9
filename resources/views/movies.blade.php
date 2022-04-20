8b0a002ec418820f20296d86deafaa2a38d5fd85
Nioni Artha Putri <nioniarthaputri@gmail.com>
2022-04-20 23:42:45 +0700

    movie list

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -19,27 +19,6 @@
                   </ul>
                 </div>
               </div>
-              {{-- <div class="mr-grid summary-row">
-                <div class="col2">
-                  <h5>SUMMARY</h5>
-                </div>
-                <div class="col2">
-                  <ul class="movie-likes">
-                    <li><i class="material-icons">&#xE813;</i>124</li>
-                    <li><i class="material-icons">&#xE813;</i>3</li>
-                  </ul>
-                </div>
-              </div> --}}
-              {{-- <div class="mr-grid">
-                <div class="col1">
-                  <p class="movie-description">{{ $movie["plot"] }}</p>
-                </div>
-              </div>
-              <div class="mr-grid actors-row">
-                <div class="col1">
-                  <p class="movie-actors">{{ $movie["actors"] }}</p>
-                </div>
-              </div> --}}
               <div class="grid-container-button">
                 <div class="watch-btn"><h3><i class="material-icons">&#xE037;</i>SEE DETAILS</h3></div>
                 <div class="action-btn"><i class="material-icons">&#xE161;</i></div>
